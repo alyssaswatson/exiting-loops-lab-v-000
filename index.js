9950e3285863cd19f8116750408926127b0100ae
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-25 00:53:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function breakOut(array, changeValue, stopValue) {
+  for (var i = 0, l = array.length; i < l ; i++ ) {
+     if (array[i] === stopValue) {
+       break
+     }
+     array[i] = changeValue
+   }
+   return array
+ }
 
 }
 function keepGoing(array, changeValue, skipValue) {
