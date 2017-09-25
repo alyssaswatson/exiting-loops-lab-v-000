1ba1f0e2f93f7ee75e58255963f29df8095fb7e3
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-25 01:00:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,10 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-
-}
+   for (let i = 0, l = array.length; i < l; i++) {
+     if (findFn(array[i])) {
+       return array[i]
+     }
+   }
+   return null
+ }
