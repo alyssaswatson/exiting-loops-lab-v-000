67115fa864aa351c013d2b76eb45b0898f10a2eb
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-25 00:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function keepGoing(array, changeValue, skipValue) {
   for (var i = 0, l = array.length; i < l ; i++ ) {
      if (array[i] === skipValue) {
        continue
+     } if (array[i] === stopValue) {
+        break
      }
      array[i] = changeValue
-   } if (array[i] === stopValue) {
-      break
-   }
+   } 
    return array
 }
 
